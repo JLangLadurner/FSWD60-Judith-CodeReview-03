91a255cc547ad43bed7ec2ebeee62b3c9cb1c58c
Judith <judithmarialang@gmail.com>
2019-01-26 16:23:15 +0100

    final commit

---
+++ Jasmin/spec_jasmin.js
@@ -0,0 +1,16 @@
+
+
+// function should return value from option items:
+
+var selection = function sled(){
+	var country = document.getElementById['country'];
+	var country= country.option[country.selectedIndex].value;
+
+console.log(field + ".");
+	
+};
+
+
+
+
+    
---
+++ Jasmin/src_jasmin.js
@@ -0,0 +1,13 @@
+
+
+
+
+var selection = function sled(){
+	var country = document.getElementById['country'];
+	var country= country.option[country.selectedIndex].value;
+};
+
+
+console.log(+ field + ".");
+
+    
